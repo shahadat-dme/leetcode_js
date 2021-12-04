4d5e3e3106b84edd7c76a841d3e1a1b4af2c6e93
Shahadat <shahadat2cp17@gmail.com>
2021-12-04 22:48:05 +0600

    13

---
+++ 13.js
@@ -0,0 +1,29 @@
+var romanToInt = function(s) {
+    const hints = {
+        I:1,
+        V:5,
+        X:10,
+        L:50,
+        C:100,
+        D:500,
+        M:1000
+    }
+
+let converted = 0;
+
+for (let i = 0; i < s.length; i++) {
+    const curr = s[i];
+    const next = s[i+1]
+     
+   if (hints[curr] < hints[next]) {
+       const num = hints[next] - hints[curr];
+       converted += num;
+       i++;
+    } else {
+        converted += hints[curr];
+    }
+}
+return converted;
+}
+
+console.log(romanToInt(["III"]))
