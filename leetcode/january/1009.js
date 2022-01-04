20ccd95164234683c5724eb653d779e22d424c43
Shahadat <shahadat2cp17@gmail.com>
2022-01-04 15:45:39 +0600

    1009

---
+++ leetcode/january/1009.js
@@ -0,0 +1,12 @@
+// const bitwiseComplement = n => (1 << (Math.floor(Math.log2(n || 1)) + 1)) - 1 - n;
+
+// const bitwiseComplement = (N) => parseInt([...N.toString(2)].map((b) => b ^ 1).join``, 2);
+
+var bitwiseComplement = function(n) {
+    let bn = n.toString(2);
+    bn = bn.split("").map(x => {
+        return (x == 1) ? 0 : 1;
+    }).join("");
+    return parseInt(bn, 2);
+};
+console.log(bitwiseComplement(5))
