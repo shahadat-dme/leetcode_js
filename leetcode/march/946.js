03ba7bee8a4bac9a5fcb21335654919aab8ce78d
Shahadat <shahadat2cp17@gmail.com>
2022-03-16 21:55:41 +0600

    946

---
+++ leetcode/march/946.js
@@ -0,0 +1,14 @@
+const validateStackSequences = function(pushed, popped) {
+    const stack = []
+    let i = 0
+
+    for (const el of pushed) {
+        stack.push(el)
+        while (stack.length && stack[stack.length - 1] === popped[i]) {
+            stack.pop()
+            i++
+        }
+    }
+    
+    return pushed.length === i
+}
