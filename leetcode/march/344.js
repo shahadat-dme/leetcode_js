f7ec4b40e6227fd31664ec0a046fef7155b24ad2
Shahadat <shahadat2cp17@gmail.com>
2022-04-02 20:02:35 +0600

    680

--- leetcode/march/344.js
+++ leetcode/march/344.js
@@ -1,15 +1,15 @@
-// var reverseString = function(s) {
+var reverseString = function(s) {
     
-//     let left = 0;
-//     let right = s.length - 1;
+    let left = 0;
+    let right = s.length - 1;
 
-//     while (left <= right) {
-//         [s[left],s[right]] = [s[right],s[left]]
-//         left++;
-//         right--;
-//     }
-//     return s;
-// };
+    while (left <= right) {
+        [s[left],s[right]] = [s[right],s[left]]
+        left++;
+        right--;
+    }
+    return s;
+};
 
 
 // typescript
