0dac6a8b7ece1f533c2dab3f72f5ab58e0dd9c3d
Shahadat <shahadat2cp17@gmail.com>
2022-07-03 23:20:02 +0600

    376

---
+++ leetcode/july/376.js
@@ -0,0 +1,13 @@
+var wiggleMaxLength = function(nums) {
+    let inc = 1, dec = 1;
+    
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] > nums[i-1]) 
+            inc = dec + 1;
+        
+        else if(nums[i] < nums[i-1])
+            dec = inc + 1;
+    }
+    
+    return Math.max(inc, dec);
+};
